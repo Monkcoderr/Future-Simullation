aa41b214678aeca3169258fb47f97c27d85f0eb3
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-13 23:16:41 +0530

    registerUser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,10 +13,6 @@ export const registerUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword
-    });
+    
 
     
