2329896931d44af50c7d60c99f2cd847cc5b4b26
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-13 23:17:30 +0530

    registerUser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,15 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
 
     
 
