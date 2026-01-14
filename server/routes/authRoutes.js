3933b8900fa711c6e36ceb20d26fd74638cba1d9
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-14 22:46:26 +0530

    fix: remove unused test endpoint from authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,9 +4,7 @@ import { registerUser } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.get("/test", (req, res) => {
-  res.send("Auth route working");
-});
+
 
 
 
