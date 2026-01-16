11ade65ee989ad32153f226fd9c87b928560438e
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-16 21:01:00 +0530

    feat: enhance createSimulation to generate and save career paths

--- server/controllers/simulationController.js
+++ server/controllers/simulationController.js
@@ -1,5 +1,7 @@
 import Simulation from "../models/Simulation.js";
 import Profile from "../models/Profile.js";
+import CareerPath from "../models/CareerPath.js";
+import { generateCareerPaths } from "../services/aiService.js";
 
 export const createSimulation = async (req, res) => {
   try {
@@ -20,7 +22,21 @@ export const createSimulation = async (req, res) => {
       }
     });
 
-    res.json(simulation);
+    
+    const careerPaths = await generateCareerPaths(simulation.input);
+
+    const savedPaths = await CareerPath.insertMany(
+      careerPaths.map(path => ({
+        ...path,
+        simulationId: simulation._id
+      }))
+    );
+
+    res.json({
+      simulation,
+      careerPaths: savedPaths
+    });
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
