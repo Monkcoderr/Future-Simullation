51f12ecded1e0d41766a5dfb60a358b5b8cde5e5
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-17 23:52:00 +0530

    feat: add schema definition for savedPath model to store simulation results

--- server/models/savedpath.js
+++ server/models/savedpath.js
@@ -13,3 +13,18 @@ export const generateCareerPaths = async (simulationInput) => {
     }
   ];
 };
+{
+    simulationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Simulation",
+      required: true
+    },
+    title: String,
+    riskLevel: String,
+    requiredSkills: [String],
+    timeline: [
+      {
+        year: String,
+        role: String,
+        salary: String
+      }
