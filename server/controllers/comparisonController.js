9e3bfff58727e6fc70054c6b8c528b036698ec90
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-20 23:24:32 +0530

    feat: add compareSimulation function to handle simulation comparisons

--- server/controllers/comparisonController.js
+++ server/controllers/comparisonController.js
@@ -1,3 +1,13 @@
 import CareerPath from "../models/CareerPath.js";
 import Profile from "../models/Profile.js";
 import { compareCareerPaths } from "../services/comparisonService.js";
+
+export const compareSimulation = async (req, res) => {
+  try {
+    const { simulationId } = req.params;
+    const userId = req.userId;
+
+    const profile = await Profile.findOne({ userId });
+    if (!profile) {
+      return res.status(400).json({ message: "Profile not found" });
+    }
