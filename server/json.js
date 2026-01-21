6836b43886b6bf890a3bd2469c6c814682078b93
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-21 23:08:51 +0530

    feat: add json file for ranked career paths with earning potential, growth speed, risk score, and skill gap

---
+++ server/json.js
@@ -0,0 +1,28 @@
+{
+  "rankedPaths": [
+    {
+      "title": "Front-End Engineer to Tech Lead",
+      "earningPotential": 28,
+      "growthSpeed": "2.88",
+      "riskScore": 1,
+      "skillGap": 4,
+      "finalScore": 0.82
+    },
+    {
+      "title": "UI/UX Developer to Product Manager",
+      "earningPotential": 28,
+      "growthSpeed": "2.75",
+      "riskScore": 2,
+      "skillGap": 6,
+      "finalScore": 0.69
+    },
+    {
+      "title": "Startup CTO",
+      "earningPotential": 35,
+      "growthSpeed": "3.87",
+      "riskScore": 3,
+      "skillGap": 9,
+      "finalScore": 0.61
+    }
+  ]
+}
