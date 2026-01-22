7a53082e06abb0af2c1b94d97076e2ce69ea9d44
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-22 23:49:04 +0530

    refactor: remove commented-out JSX example from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App 
-    // This is NOT HTML. It's JSX, which gets turned into JavaScript.
-const myName = "Alex";
-const element = <h1 className="greeting">Hello, {myName}!</h1>;
-// The {myName} part is JavaScript being embedded./>
+    />
   </StrictMode>,
 )
