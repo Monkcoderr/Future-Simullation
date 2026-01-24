7fc9b2db42212370757d47fd37371232ac9e2579
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-24 23:50:11 +0530

    feat: move futureguess functionality into AI_reloading function

--- server/services/forecasting.js
+++ server/services/forecasting.js
@@ -2,11 +2,12 @@ const forecasting = ()=>{
     removeEventListener.apply(farecasting )
 }
 
-function futureguess(){
-    SVGAElement.bind()
-    XMLHttpRequestUpload()
-}
+
 
 const AI_reloading = ()=>{
     Response(extends)
+    function arrange(){
+    SVGAElement.bind()
+    XMLHttpRequestUpload()
+}
 }
