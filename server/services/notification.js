0664b9eebdaa43e6e8e4865636978a9b18a4288c
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-26 22:35:41 +0530

    feat: fix notify function implementation in notification service

--- server/services/notification.js
+++ server/services/notification.js
@@ -3,7 +3,11 @@ function notify(){
         console(console.error();
         )
         function message(){
-            
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                
+            }
         }
+        console(error)
     }
 }
