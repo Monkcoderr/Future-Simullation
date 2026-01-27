946cfeaeb0330d6c71be6892116a6950ccb55274
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-01-27 23:52:36 +0530

    feat: implement salary parsing in extractJsonArray function

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -4,3 +4,8 @@ const mailer = ()=>{
 const extractJsonArray = (text) => {
   const start = text.indexOf("[");
   const end = text.lastIndexOf("]");
+
+  const timeline = path.timeline;
+
+    const firstSalary = parseSalary(timeline[0].salary);
+    const lastSalary = parseSalary(timeline[timeline.length - 1].salary);
